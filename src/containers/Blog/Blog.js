865a623b5d737bf21756f01360d22014c63ee6e5
Aron Bergman <brgmn@icloud.com>
2020-04-10 20:27:11 +0300

    Setting Up Links

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,7 +1,5 @@
-import React, { Component } from 'react';
-// import axios from 'axios';
+import React, {Component} from 'react';
 import axios from '../../axios';
-
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
@@ -12,11 +10,11 @@ class Blog extends Component {
         posts: [],
         selectedPostId: null,
         error: false
-    }
+    };
 
-    componentDidMount () {
-        axios.get( '/posts' )
-            .then( response => {
+    componentDidMount() {
+        axios.get('/posts')
+            .then(response => {
                 const posts = response.data.slice(0, 4);
                 const updatedPosts = posts.map(post => {
                     return {
@@ -26,7 +24,7 @@ class Blog extends Component {
                 });
                 this.setState({posts: updatedPosts});
                 // console.log( response );
-            } )
+            })
             .catch(error => {
                 // console.log(error);
                 this.setState({error: true});
@@ -35,30 +33,38 @@ class Blog extends Component {
 
     postSelectedHandler = (id) => {
         this.setState({selectedPostId: id});
-    }
+    };
 
-    render () {
+    render() {
         let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>;
         if (!this.state.error) {
             posts = this.state.posts.map(post => {
-                return <Post 
-                    key={post.id} 
-                    title={post.title} 
+                return <Post
+                    key={post.id}
+                    title={post.title}
                     author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)} />;
+                    clicked={() => this.postSelectedHandler(post.id)}/>;
             });
         }
 
         return (
-            <div>
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/new-post">New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
                 <section className="Posts">
                     {posts}
                 </section>
                 <section>
-                    <FullPost id={this.state.selectedPostId} />
+                    <FullPost id={this.state.selectedPostId}/>
                 </section>
                 <section>
-                    <NewPost />
+                    <NewPost/>
                 </section>
             </div>
         );
